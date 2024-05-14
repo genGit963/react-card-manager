e3ee8f8b99a3334fe675a5db413e56532fca282d
Mahesh Bogati <117003503+genGit963@users.noreply.github.com>
2024-05-14 23:45:07 +0545

    animation and styling configured

--- src/components/card-manager.tsx
+++ src/components/card-manager.tsx
@@ -13,7 +13,7 @@ const CardManagerTailwind: React.FC<CardTypes> = ({
 }) => {
   return (
     <div
-      className={`grid grid-cols-auto-fill-200 auto-rows-minmax-200-1fr auto-cols-minmax-200-1fr gap-x-4 gap-y-4 overflow-hidden`}
+      className={`grid grid-cols-auto-fill-200 auto-rows-minmax-200-1fr auto-cols-minmax-200-1fr gap-x-4 gap-y-4 overflow-hidden animate-slide-${animation_style}`}
     >
       {children}
     </div>
