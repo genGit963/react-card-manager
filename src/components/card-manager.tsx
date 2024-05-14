13288c9a348e25f7c9c8d83501c43261f3ffd3c5
Mahesh Bogati <117003503+genGit963@users.noreply.github.com>
2024-05-14 23:42:29 +0545

    All setup is working

---
+++ src/components/card-manager.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { ReactNode } from "react";
+import "./custom.css";
+
+type CardTypes = {
+  animation_style: "from-left" | "from-right" | "from-bottom" | "from-top";
+  children: ReactNode;
+};
+
+const CardManagerTailwind: React.FC<CardTypes> = ({
+  animation_style = "from-bottom",
+  children,
+}) => {
+  return (
+    <div
+      className={`grid grid-cols-auto-fill-200 auto-rows-minmax-200-1fr auto-cols-minmax-200-1fr gap-x-4 gap-y-4 overflow-hidden`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default CardManagerTailwind;
